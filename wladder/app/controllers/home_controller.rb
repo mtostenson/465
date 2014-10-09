0f628200e20d629d37a21213f3263d7e7b5118f4
mt523 <mtostenson@mail.csuchico.edu>
2014-10-09 05:32:51 +0000

    wladder is now working

--- wladder/app/controllers/home_controller.rb
+++ wladder/app/controllers/home_controller.rb
@@ -6,8 +6,34 @@ class HomeController < ApplicationController
       $random1 = dictionary.sample
       $random2 = dictionary.sample
    end
-   def result
-      @params = params
-      $result = false      
+   def result       
+	   @guesses = Array.new
+	   if params[:one] != ""
+	      @guesses.push params[:one]
+	   end
+	   if params[:two] != ""
+	      @guesses.push params[:two]
+	   end
+	   if params[:three] != ""
+	      @guesses.push params[:three]
+	   end
+	   if params[:four] != ""
+	      @guesses.push params[:four]
+	   end
+	   if params[:five] != ""
+	      @guesses.push params[:five]
+	   end
+	   @guesses.each_with_index do |guess, i|
+	      $winner = legal @guesses
+	      if guess == @guesses.first
+	         $winner = edit_distance_of_one($random1, guess)
+	      elsif guess == @guesses.last
+	         $winner = edit_distance_of_one(guess, $random2)
+	      else $winner = edit_distance_of_one(guess, @guesses[i+1])
+	      end
+	   end
+	   if not edit_distance_of_one($random1, params[:one])
+	      $winner = false   
+	   end	   
    end
 end

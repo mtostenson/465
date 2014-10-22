7bd6c08508275a925f5685493b13551df2679358
mt523 <mtostenson@mail.csuchico.edu>
2014-10-22 14:37:16 +0000

    Initial scorebook commit

---
+++ scorebook/app/controllers/high_scores_controller.rb
@@ -0,0 +1,8 @@
+class HighScoresController < ApplicationController
+
+    #GET /high_scores
+    def index
+        @high_scores = HighScore.all
+    end
+
+end
---
+++ scorebook/config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+    
+    root 'high_scores#index'
+    
+end

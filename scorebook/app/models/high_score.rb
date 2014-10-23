c52ada37a419cebe2766a7a4dab79bd36f7dd93b
mt523 <mtostenson@mail.csuchico.edu>
2014-10-23 09:57:29 +0000

    Adds doi_server project

--- scorebook/app/models/high_score.rb
+++ scorebook/app/models/high_score.rb
@@ -1,2 +1,7 @@
 class HighScore < ActiveRecord::Base
+
+    validates :user, presence: true
+    validates :game, presence: true
+    validates :score, presence: true, numericality: {greateer_than_or_equal_to: 0}
+
 end;

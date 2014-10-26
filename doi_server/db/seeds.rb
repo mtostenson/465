01fce6f2c905b46c06907c98aef9a817e9117b97
mt523 <mtostenson@mail.csuchico.edu>
2014-10-26 04:17:52 +0000

    Pre-publish commit

--- doi_server/db/seeds.rb
+++ doi_server/db/seeds.rb
@@ -8,8 +8,11 @@
 
 dois = Doi.create(
     [
-        { name: 'Google', desc: 'Google search engine', doi_num: '33J2LK4' },
-        { name: 'Reddit', desc: 'Reddit website', doi_num: 'ASDFBZSCV' }
+        { name: 'Google', desc: 'Google search engine', doi_num: 'da90a424ae3f47d70a2c' },
+        { name: 'Reddit', desc: 'Reddit website', doi_num: '7edbe3e5806b083c3c49' },
+        { name: 'Amazon', desc: 'Shopping and stuff', doi_num: '32aa746b613ae7589a5c' },
+        { name: 'Yahoo', desc: 'The old Google', doi_num: '35b8e72959fc9492631c' },
+        { name: 'Ebay', desc: 'The auction site', doi_num: 'e72435b892631c959fc9' }
     ]
 )
 
@@ -18,5 +21,14 @@ urls = Url.create(
         { url: 'www.google.com', doi_id: dois[0].id },
         { url: 'www.gooogle.com', doi_id: dois[0].id },
         { url: 'www.reddit.com', doi_id: dois[1].id },
+        { url: 'www.amazon.com', doi_id: dois[2].id },
+        { url: 'www.amazon2.com', doi_id: dois[2].id },
+        { url: 'www.amazon3.com', doi_id: dois[2].id },
+        { url: 'www.amazon4.com', doi_id: dois[2].id },
+        { url: 'www.yahoo.com', doi_id: dois[3].id },
+        { url: 'www.yahoooooo.com', doi_id: dois[3].id },
+        { url: 'www.ebay.com', doi_id: dois[4].id },
+        { url: 'www.e-bay.com', doi_id: dois[4].id },
+        { url: 'www.eeebay.com', doi_id: dois[4].id }
     ]
 )

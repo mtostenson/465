045fc7154b59a94c818ba13015adaedb96da93e0
mt523 <mtostenson@mail.csuchico.edu>
2014-11-16 01:27:08 +0000

    Attempting using scaffolding...

--- img/app/models/image.rb
+++
@@ -1,9 +0,0 @@
-class Image < ActiveRecord::Base
-
-    has_many :tags, dependent: :destroy
-    
-    def name
-        filename
-    end
-
-end
--- img/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-
-    resources :images do
-        resources :tags, shallow: true
-    end
-    
-    root 'images#index'
-
-end

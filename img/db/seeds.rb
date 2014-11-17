50f973e5c9cec4dced9535556a41bc127d0ae621
mt523 <mtostenson@mail.csuchico.edu>
2014-11-17 07:44:46 +0000

    adds SEEEEEEEEDS :D

--- img/db/seeds.rb
+++ img/db/seeds.rb
@@ -1,8 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
+User.create!([
+  {email: "a@a.com", password: "a", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 5, current_sign_in_at: "2014-11-17 07:15:33", last_sign_in_at: "2014-11-17 07:07:25", current_sign_in_ip: "50.173.88.142", last_sign_in_ip: "50.173.88.142", name: "a"},
+  {email: "b@b.com", password: "b", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 2, current_sign_in_at: "2014-11-17 07:22:18", last_sign_in_at: "2014-11-17 07:15:01", current_sign_in_ip: "50.173.88.142", last_sign_in_ip: "50.173.88.142", name: "b"}
+])
+Image.create!([
+  {filename: "WLFYCKNU.jpg", priv: true, user_id: 1, tag_id: nil},
+  {filename: "RFTFGBIO.jpg", priv: false, user_id: 1, tag_id: nil},
+  {filename: "WKMNSVON.jpg", priv: false, user_id: 1, tag_id: nil},
+  {filename: "TDAHQJYM.jpg", priv: true, user_id: 1, tag_id: nil}
+])
+ImageUser.create!([
+  {image_id: nil, user_id: 2},
+  {image_id: nil, user_id: 2},
+  {image_id: nil, user_id: 2},
+  {image_id: nil, user_id: 2}
+])
+Tag.create!([
+  {tag: "This is a tag", image_id: 2},
+  {tag: "another tag", image_id: 2}
+])
